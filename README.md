d8d343d4ad34484ac2b00d695dae3df9af16ea26
Dimitri Pertin <dimitri.pertin@inria.fr>
2020-04-26 11:58:06 +0200

    Add an automation script to build and update
    The script `update_pkg.sh` can be used to automate the package building and
    updating processes. Its workflow is as follows:
    1. Check if a new version is available from the original repository (or build a
       specific version if '-v' argument is provided);
    2. Clone the related AUR repo in a new directory and move to it;
    3. Update the required files:
       - Update `PKGBUILD` (pkgver, md5sums)
       - Update `.SRCINFO`
    4. Try to build the package:
       - If success and set to update: the script commits and push to GitHub. From
         there, Drone CI will try to build the package on a bare ArchLinux container.
       - If failure: send an email.
    
    ```
    ./update_pkg.sh usage:
          -h | --help)        # Display help.
          -f | --file)        # Path to a PKGBUILD file (default: "./PKGBUILD").
          -u | --update)      # Update GitHub repository (default: false).
          -v | --version)     # If a specific semantic version is required (e.g. 0.8.5).
          -V | --verbose)     # Enable verbose mode (default: false).
    ```

--- README.md
+++ README.md
@@ -8,3 +8,27 @@ This repo contains the required files to generate an Archlinux package of
 a lightweight music streaming server written in Go, which implements the
 [Subsonic API](http://www.subsonic.org/pages/api.jsp).
 
+## Automation script
+
+The script `update_pkg.sh` can be used to automate the package building and
+updating processes. Its workflow is as follows:
+1. Check if a new version is available from the original repository (or build a
+   specific version if '-v' argument is provided);
+2. Clone the related AUR repo in a new directory and move to it;
+3. Update the required files:
+   - Update `PKGBUILD` (pkgver, md5sums)
+   - Update `.SRCINFO`
+4. Try to build the package:
+   - If success and set to update: the script commits and push to GitHub. From
+     there, Drone CI will try to build the package on a bare ArchLinux container.
+   - If failure: send an email.
+
+```
+./update_pkg.sh usage:
+      -h | --help)        # Display help.
+      -f | --file)        # Path to a PKGBUILD file (default: "./PKGBUILD").
+      -u | --update)      # Update GitHub repository (default: false).
+      -v | --version)     # If a specific semantic version is required (e.g. 0.8.5).
+      -V | --verbose)     # Enable verbose mode (default: false).
+```
+
---
+++ update_pkg.sh
@@ -0,0 +1,140 @@
+#!/usr/bin/env bash
+
+usage() { echo "$0 usage:" && grep " -. |.*) *#" $0; }
+
+
+# set default values
+pkgbuildFile="./PKGBUILD"
+updateGithubRepo=false
+repoPkgVer=$(curl --silent "https://api.github.com/repos/sentriz/gonic/tags" | \
+    jq -r '.[0].name' | cut -d 'v' -f 2)
+verbose=false
+
+# parse arguments
+GET_OPT=$(getopt \
+    -o 'f:uv:Vh' \
+    --long 'file:,update,version:,verbose,help' \
+    -- "$@")
+eval set -- "${GET_OPT}"
+
+while true; do
+    case "$1" in
+      -h | --help)        # Display help.
+          usage; exit 0;;
+      -f | --file)        # Path to a PKGBUILD file (default: "./PKGBUILD").
+          pkgbuildFile=$2; shift 2;;
+      -u | --update)      # Update GitHub repository (default: false).
+          updateGithubRepo=true; shift 1;;
+      -v | --version)     # If a specific semantic version is required (e.g. 0.8.5).
+          repoPkgVer=$2; shift 2;;
+      -V | --verbose)     # Enable verbose mode (default: false).
+          verbose=true; shift 1;;
+      --) shift; break;;
+      *) usage; exit 1;;
+    esac
+done
+
+# check if given PKGBUILD file exists
+if [[ ! -f "$pkgbuildFile" ]]
+then
+    echo "\"${pkgbuildFile}\" is not a valid PKGBUILD file (not exists)"
+    exit 1
+fi
+
+# check if given version is valid
+if [[ ! $repoPkgVer =~ ^[0-9]+\.[0-9]+ ]]
+then
+    echo "\"$2\" is not a valid version number (e.g. 0.8.5)"
+    exit 1
+fi
+
+source ${pkgbuildFile}
+
+
+# Compare two semantic version, inspired from
+# https://stackoverflow.com/questions/4023830/how-to-compare-two-strings-in-dot-separated-version-format-in-bash
+vercomp () {
+    if [[ $1 == $2 ]]
+    then
+        return 0
+    fi
+    local IFS=.
+    local i ver1=($1) ver2=($2)
+    # fill empty fields in ver1 with zeros
+    for ((i=${#ver1[@]}; i<${#ver2[@]}; i++))
+    do
+        ver1[i]=0
+    done
+    for ((i=0; i<${#ver1[@]}; i++))
+    do
+        if [[ -z ${ver2[i]} ]]
+        then
+            # fill empty fields in ver2 with zeros
+            ver2[i]=0
+        fi
+        if ((10#${ver1[i]} < 10#${ver2[i]}))
+        then
+            return 0
+        fi
+    done
+    return 1
+}
+
+update_package () {
+    # update package version
+    sed "s/^pkgver=.*/pkgver=${repoPkgVer}/" -i ${pkgbuildFile}
+
+    # update md5 sums
+    if hash updpkgsums 2>/dev/null; then
+        updpkgsums
+    else
+        md5sums=$(makepkg -g -p ${pkgbuildFile})
+        perl -p0e \
+            "s/md5sums=(.*)\n\n\nbuild/${md5sums}\n\n\nbuild/s" \
+            -i ${pkgbuildFile}
+    fi
+
+    # update .SRCINFO
+    mksrcinfo
+}
+
+${verbose} && echo "Current AUR version: ${pkgver}"
+${verbose} && echo "Main repository version: ${repoPkgVer}"
+
+if vercomp ${pkgver} ${repoPkgVer};
+then
+    dir_name=$"${pkgname}_${repoPkgVer}"
+
+    echo "[OUT_OF_DATE] New version available"
+    echo "Try to build the new package under ${dir_name}"
+    [ -d "${dir_name}" ] && rm -rf ${dir_name}
+    git clone https://aur.archlinux.org/${pkgname}.git ${dir_name}
+    sed \
+        "s#https://aur.archlinux.org#ssh+git://aur@aur.archlinux.org#" \
+        -i .git/config
+
+    pushd ${dir_name}
+    update_package
+
+    if makepkg_log=$(makepkg -p ${pkgbuildFile} 2>&1)
+    then
+        echo "[SUCCESS] Package was successfully built"
+        # if update is set, commit and copy indexed files to github directory
+        ${updateGithubRepo} && git commit -am "Update to ${repoPkgVer}"
+        ${updateGithubRepo} && cp $(git ls-files) ../
+    else
+        echo "[FAILURE] Package building failed"
+
+        # Send an email
+        content="Updating from ${pkgver} to ${repoPkgVer}\n\n
+        This is the output of makepkg:\n${makepkg_log}"
+        echo ${content} | mail -v -s "[${pkgname}] makepkg failed"
+    fi
+    popd
+    # if update is set, commit and push to GitHub
+    ${updateGithubRepo} && git commit -am "Update to ${repoPkgVer}"
+    #${updateGithubRepo} && git push origin master
+else
+    echo "[UPDATED] Package is up to date"
+    # Nothing to do
+fi
